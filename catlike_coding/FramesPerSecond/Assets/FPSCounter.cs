dba61c83e82f0dfbe8a89e2947a6bf12efd76ab1
Julia Schwarz <julenka@cs.cmu.edu>
2016-05-14 14:13:42 -0700

    FPS initial commit

---
+++ catlike_coding/FramesPerSecond/Assets/FPSCounter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSCounter : MonoBehaviour
+{
+    public int FPS { get; private set; }
+
+    private void Update()
+    {
+        FPS = (int)(1f / Time.unscaledDeltaTime);
+    }
+}
---
+++ catlike_coding/FramesPerSecond/Assets/NucleonSpawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NucleonSpawner : MonoBehaviour
+{
+    public float timeBetweenSpawns;
+    public float spawnDistance;
+    public Nucleon[] nucleonPrefabs;
+
+    float timeSinceLastSpawn;
+    private void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn > timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
+    }
+
+    private void SpawnNucleon()
+    {
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+    }
+}
