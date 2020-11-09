7f8ae0ecc768a4ba7e77cab96f2698bab24632cb
Julia Schwarz <julenka@cs.cmu.edu>
2020-11-09 14:32:33 -0800

    finish mathematical surfaces tutorial

--- catlike_coding/Graphs/Assets/Scripts/Graph.cs
+++ catlike_coding/Graphs/Assets/Scripts/Graph.cs
@@ -14,34 +14,29 @@ public class Graph : MonoBehaviour
     {
         points = new Transform[resolution * resolution];
         float step = 2f / resolution;
-        Vector3 position = default, scale = Vector3.one * step;
+        Vector3 scale = Vector3.one * step;
         for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
-            if (x == resolution){
-                x = 0;
-                z++;
-            }
-
             Transform point = Instantiate(pointPrefab);
-            position.x = (x + 0.5f) * step - 1f;
-            position.z = (z + 0.5f) * step - 1f;
-            point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
             points[i] = point;
         }
     }
 
-    void Update()
-    {
-        float time = Time.time;
-        for (int i = 0; i < points.Length; i++)
-        {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-            FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
-            position.y = f(position.x, position.z, time);
-            point.position = position;
-        }
-    }
+	void Update () {
+		FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+		float time = Time.time;
+		float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+			if (x == resolution) {
+				x = 0;
+				z += 1;
+                v = (z + 0.5f) * step - 1f;
+			}
+			float u = (x + 0.5f) * step - 1f;
+			points[i].localPosition = f(u, v, time);
+		}
+	}
 }
