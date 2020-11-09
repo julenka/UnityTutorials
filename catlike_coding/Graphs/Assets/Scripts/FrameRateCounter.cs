97c2319c3321bdb390a215fd9bd1399d429f1203
Julia Schwarz <julenka@cs.cmu.edu>
2020-11-09 15:59:40 -0800

    add missing files from perf section and start on compute shaders

---
+++ catlike_coding/Graphs/Assets/Scripts/FrameRateCounter.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class FrameRateCounter : MonoBehaviour
+{
+
+    [SerializeField]
+    TextMeshProUGUI display = default;
+    int frames;
+
+    float duration, bestDuration = float.MaxValue, worstDuration;
+
+    [SerializeField, Range(0.1f, 2f)]
+    float sampleDuration = 1f;
+
+    public enum DisplayMode { FPS, MS }
+
+    [SerializeField]
+    DisplayMode displayMode = DisplayMode.FPS;
+    void Update()
+    {
+        float frameDuration = Time.unscaledDeltaTime;
+        frames += 1;
+        duration += frameDuration;
+        if (frameDuration < bestDuration)
+        {
+            bestDuration = frameDuration;
+        }
+        if (frameDuration > worstDuration)
+        {
+            worstDuration = frameDuration;
+        }
+        if (duration >= sampleDuration)
+        {
+			if (displayMode == DisplayMode.FPS) {
+				display.SetText(
+					"FSP\n{0:0}\n{1:0}\n{2:0}",
+					1f / bestDuration,
+					frames / duration,
+					1f / worstDuration
+				);
+			}
+			else {
+				display.SetText(
+					"MS\n{0:0}\n{1:0}\n{2:0}",
+					1000f * bestDuration,
+					1000f * duration / frames,
+					1000f * worstDuration
+				);
+			}
+            frames = 0;
+            duration = 0f;
+            bestDuration = float.MaxValue;
+            worstDuration = 0f;
+        }
+    }
+}
---
+++ catlike_coding/Graphs/Assets/Scripts/GPUGraph.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+
+public class GPUGraph : MonoBehaviour
+{
+
+    int resolution = 10;
+    [SerializeField]
+    FunctionLibrary.FunctionName function = default;
+    [SerializeField, Min(0f)]
+    float functionDuration = 1f, transitionDuration = 1f;
+
+    public enum TransitionMode { Cycle, Random }
+
+    [SerializeField]
+    TransitionMode transitionMode = TransitionMode.Cycle;
+    float duration;
+    bool transitioning;
+
+    FunctionLibrary.FunctionName transitionFunction;
+
+    ComputeBuffer positionsBuffer;
+
+    void OnEnable()
+    {
+        positionsBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);
+    }
+
+    void OnDisable()
+    {
+        positionsBuffer.Release();
+        positionsBuffer = null;
+    }
+
+    void Update()
+    {
+        duration += Time.deltaTime;
+        if (transitioning)
+        {
+            if (duration >= transitionDuration)
+            {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
+        }
+        else if (duration >= functionDuration)
+        {
+            duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
+            PickNextFunction();
+        }
+
+    }
+
+    void PickNextFunction()
+    {
+        function = transitionMode == TransitionMode.Cycle ?
+            FunctionLibrary.GetNextFunctionName(function) :
+            FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+    }
+
+}
